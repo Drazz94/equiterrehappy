6f1dffbc899c5740ee505b7428562e8e6fc18866
julioagp <agopian@intechinfo.fr>
2017-12-18 16:03:23 +0100

    page de deconnexion

--- v/deconnecter.php
+++ v/deconnecter.php
@@ -3,12 +3,18 @@
 <head>
     <meta charset="utf-8">
     <title>Deconnexion</title>
-    </head>
+    <link rel="stylesheet" href="../css/deco.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>    </head>
 
     <body>
-        VOUS ETES DECO
+        <p>
+        VOUS ETES DECONNECTÉ
         <form action="/eth/index.html" method="post">
-            <input type="submit" name="valider" value="retour">
+            <input class="btn btn-primary btn-lg"  type="submit" name="valider" value="retour" >
         </form>
+           
+        
     </body>
 </html>
